a7826578490764fd3b4f6735791cc07527786853
Mats Thys <mats.thys@gmail.com>
2021-03-28 16:29:56 +0200

    gitignore fix

--- settings/settings.php
+++ settings/settings.php
@@ -4,9 +4,6 @@
     'DB_USERNAME'=>'legato',
     'DB_PASSWORD'=>'94CbkEBEoFy4MP',
     'DB_DATABASE'=>'legato',
-    'DB_PORT' => '13306',
-    'DB_TEST' => 'test',
-    'DB_TEST' => 'test',
-    'DB_TEST' => 'qsdfsqdfdsqdfqds'
+    'DB_PORT' => '13306'
     );
 ?>
