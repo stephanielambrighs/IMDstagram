f1e4ed0f6bd0923a8c51a15d9ce188adb7cf8cdc
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-03 11:42:08 +0200

    add logo in navbar

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <img src="/images/logo-02.svg" alt="legato_logo">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
