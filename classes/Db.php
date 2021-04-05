066657fedeb40bb1066cb8dee7a6beb6483dcce0
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-05 23:05:39 +0200

    fixed db load_once issues

--- classes/Db.php
+++ classes/Db.php
@@ -1,27 +1,24 @@
 <?php
+
 require_once("autoload.php");
 
-class Db
-{
+class Db {
 
     private static $conn;
 
     public static function getConnection()
     {
-        include_once(__DIR__ . "/../settings/settings.php");
-        $host = $config['DB_HOST'];
-        // var_dump($host);
-        $dbname = $config['DB_DATABASE'];
-        // var_dump($dbname);
-        $port = $config['DB_PORT'];
+        include(__DIR__ . "/../settings/settings.php");
         if (self::$conn === null) {
+            $host = $config['DB_HOST'];
+            $dbname = $config['DB_DATABASE'];
+            $port = $config['DB_PORT'];
             self::$conn = new PDO(
                 "mysql:host=$host;dbname=$dbname;port=$port",
                 $config['DB_USERNAME'],
                 $config['DB_PASSWORD']
             );
             $status = self::$conn->getAttribute(PDO::ATTR_CONNECTION_STATUS);
-            // var_dump($status);
             return self::$conn;
         } else {
             return self::$conn;
@@ -33,9 +30,9 @@ class Db
         $statement = $conn->prepare("SELECT id, name FROM genre");
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($statement->errorInfo());
 
         $genreList = [];
-
         foreach($result as $db_genre){
             $genre = new Genre($db_genre['id'], $db_genre['name']);
             array_push($genreList, $genre);
@@ -56,15 +53,27 @@ class Db
         $statement->bindValue(':user_id', $post->getUser_id());
         $statement->bindValue(':type_id', $post->getType_id());
         $statement->bindValue(':file_path', $post->getFile_path());
-
         $result = $statement->execute();
-        // var_dump($result);
-        // var_dump($statement->errorInfo());
+        var_dump($result);
+        var_dump($statement->errorInfo());
     }
 
     private static function get_current_time(){
         $now = new DateTime('now');
         return date_format($now, 'Y-m-d H:i:s');
     }
+
+    // public static function getGenreByName($genre_name){
+    //     var_dump($genre_name);
+    //     $conn = self::getConnection();
+    //     $statement = $conn->prepare("SELECT id, name FROM `genre` WHERE LOWER(name) = :name");
+    //     $statement->bindValue(':name', strtolower($genre_name));
+    //     $statement->execute();
+    //     $result = $statement->fetch(PDO::FETCH_ASSOC);
+    //     var_dump($result);
+    //     $genre = new Genre($result['id'], $result['name']);
+    //     var_dump($statement->errorInfo());
+    //     return $genre;
+    // }
 
 }
