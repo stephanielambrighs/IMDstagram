94422fc5fab7fd7d9a43ccb13c30187ecb027258
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-04-05 16:15:42 +0200

    Update login.php

--- login.php
+++ login.php
@@ -7,17 +7,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Login</title>
 </head>
 <body>
     <main>
-        <form action="f-login" action="" method="">
-            <a class="a-logo" href="#"><img id="logo" src="images/logo-02.svg" alt="logo-legato"></a>
-            <input type="text" class="f-text" placeholder="email" name="email">
-            <input type="text" class="f-text" placeholder="password" name="password">
-            <input type="submit" name="btn-signin" id="btn-signin" class="f-m-btn" value="Sign In">
-        </form>
+        <div class="mb-3">
+            <form action="f-login" action="" method="">
+                <a class="a-logo" href="#"><img id="logo" src="images/logo-02.svg" alt="logo-legato"></a>
+                <input type="text" class="f-text" placeholder="email" name="email">
+                <input type="text" class="f-text" placeholder="password" name="password">
+                <input type="submit" name="btn-signin" id="btn-signin" class="f-m-btn" value="Sign In">
+            </form>
+        </div>
     </main>
     <?php include_once(__DIR__."/inc/footer.inc.php") ?>
 </body>
