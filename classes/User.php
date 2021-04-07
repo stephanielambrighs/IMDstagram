60c6338923f4c93baded3e9739889298668d4469
Mats Thys <mats.thys@gmail.com>
2021-04-07 19:39:37 +0200

    updated User class

--- classes/User.php
+++ classes/User.php
@@ -5,9 +5,15 @@ class User
 {
     private $id;
     private $email;
+    private $firstname;
+    private $lastname;
     private $username;
     private $password;
-    private $passwordConf;
+    private $avatar;
+    private $bio;
+    private $dateOfBirth;
+
+    private $emailUsedError;
 
     /**
      * Get the value of id
@@ -49,6 +55,45 @@ class User
     }
 
     /**
+     * Get the value of firstname
+     */ 
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set the value of firstname
+     *
+     * @return  self
+     */ 
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastname
+     */ 
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @return  self
+     */ 
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+    /**
      * Get the value of username
      */ 
     public function getUsername()
@@ -87,62 +132,152 @@ class User
 
         return $this;
     }
-
     /**
-     * Get the value of passwordConf
+     * Get the value of avatar
      */ 
-    public function getPasswordConf()
+    public function getAvatar()
     {
-        return $this->passwordConf;
+        return $this->avatar;
     }
 
     /**
-     * Set the value of passwordConf
+     * Set the value of avatar
      *
      * @return  self
      */ 
-    public function setPasswordConf($passwordConf)
+    public function setAvatar($avatar)
     {
-        $this->passwordConf = $passwordConf;
+        $this->avatar = $avatar;
 
         return $this;
     }
 
-
-
-
-    public function registerUser()
+    /**
+     * Get the value of bio
+     */ 
+    public function getBio()
     {
-        //$conn = Db::getConnection();
+        return $this->bio;
+    }
+
+    /**
+     * Set the value of bio
+     *
+     * @return  self
+     */ 
+    public function setBio($bio)
+    {
+        $this->bio = $bio;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dateOfBirth
+     */ 
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * Set the value of dateOfBirth
+     *
+     * @return  self
+     */ 
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+
+        return $this;
+    }
+    /**
+     * Get the value of emailUsedError
+     */ 
+    public function getEmailUsedError()
+    {
+        return $this->emailUsedError;
+    }
+
+    /**
+     * Set the value of emailUsedError
+     *
+     * @return  self
+     */ 
+    public function setEmailUsedError($emailUsedError)
+    {
+        $this->emailUsedError = $emailUsedError;
+
+        return $this;
+    }
+
+    // =========================================
+    //
+    // =========================================
+
+
+    public function register()
+    {
         $conn = Db::getConnection();
-        var_dump($conn);
-
-        /**TO ADD PDO EXCEPTION CONNECTION FAILED */
-
-        /**bind values */
 
         $email = $this->getEmail();
+        $firstname = $this->getFirstname();
+        $lastname = $this->getLastname();
         $username = $this->getUsername();
-        $password = $this->getPassword();
-        $passwordConf = $this->getPasswordConf();
-        var_dump("😎" . $email . " " . $username . " " . $password . " " . $passwordConf);
-
-
+        $password = password_hash($this->getPassword(), PASSWORD_BCRYPT);
+        $avatar = $this->getAvatar();
+        $bio = $this->getBio();
+        $dateOfBirth = $this->getDateOfBirth();
        
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $statement = $conn->prepare("insert into users (email, username, password, password_conf) values(:email, :username, :password, :password_conf)");
-            $statement->bindValue(":email", $email);
-            $statement->bindValue(":username", $username);
-            $statement->bindValue(":password", $password);
-            $statement->bindValue(":password_conf", $passwordConf);
-            $statement->execute();
 
-        }
+
+       // UIT IF($_POST) HALEN
+
+       // try catch {
+       //   valid email (db)
+       //   valid date_of_birth (db)
+       //   password hash
+       // } 
+
+            // email validation
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $statement = $conn->prepare("select email from users where email = ?");
+                $statement->bindValue(1, $email);
+                $statement->execute();
+
+                if ($statement->rowCount() > 0) {
+                    echo "EMAIL IS ALREADY IN USE";
+                    $this->setEmailUsedError("Email is already in use");
+                } else {
+                    $statement = $conn->prepare("insert into users (email, username, password, img_user_directory, first_name, last_name, bio, date_of_birth) 
+                    values(:email, :username, :password, :avatar, :firstname, :lastname, :bio, :date_of_birth)");
+                    $statement->bindValue(":email", $email);
+                    $statement->bindValue(":username", $username);
+                    $statement->bindValue(":password", $password);
+                    $statement->bindValue(":avatar", $avatar);
+                    $statement->bindValue(":firstname", $firstname);
+                    $statement->bindValue(":lastname", $lastname);
+                    $statement->bindValue(":bio", $bio);
+                    $statement->bindValue(":date_of_birth", $dateOfBirth);
+                    $statement->execute();
+
+                    header('Location: completeProfile.php');
+                }
+            } else {
+                echo "NOT A VALID EMAIL";
+            }
+        
+    }
+
+    public function completeProfile()
+    {
+
+
+        
     }
 
 
+    
 
-
-
-
+    
 }
