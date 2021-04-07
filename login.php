51b18ad19a48001b286c4fb2ecfa2d21ee82434e
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-04-07 11:49:35 +0200

    Update login.php

--- login.php
+++ login.php
@@ -28,12 +28,13 @@ if(!empty($_POST)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Login</title>
 </head>
 <body>
     <main>
         <div class="mb-3">
-            <form action="login.php" method="post">
+            <form class="form" action="login.php" method="post">
                 <a class="a-logo" href="#"><img id="logo" src="images/logo-02.svg" alt="logo-legato"></a>
                 <?php if(isset($error)): ?>
                 <div class="error">The username or password is incorrect. Please try again.</div>
