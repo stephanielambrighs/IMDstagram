682d295c6dd4d73815d8c8ade1aa3ab91d21898b
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-04-10 10:51:29 +0200

    Update canLogin.php

--- classes/canLogin.php
+++ classes/canLogin.php
@@ -1,10 +1,8 @@
 <?php
 
-    include_once(__DIR__."./Db.php");
-    
-    getConnection();
+    include_once("../autoload.php");
 
-    function login($username, $password){
+    public function login($username, $password){
             
         if($username === "dev" && $password === "dev"){
             return true;
