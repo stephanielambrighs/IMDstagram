8e421e95e0208db9e16caa66693cc969bdadcb4a
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-04-12 12:03:39 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,22 @@
 <?php
     require_once("autoload.php");
+
+
+    if(!empty($_POST)){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $user = new User();
+        $user->getUsername();
+        $user->getPassword();
+
+        /*if(login($username, $password)){
+            session_start();
+            $_SESSION["username"] = $username;
+            header("Location: index.php");
+        }else{
+            $error = true;
+        }*/
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +36,7 @@
     </div>
     <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" class="form-control" id="exampleInputPassword1" name="password">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
