a55082f7f6e628e03091d9c915d9a316176ac9ee
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-04-18 17:43:10 +0200

    Update search.php

--- search.php
+++ search.php
@@ -8,6 +8,11 @@
     $searchPostInput = new Post();
     $searchPostInput->setTitle($_POST["search"]);
     $searchPostOutput = $searchPostInput->searchPost();
+
+    $searchTagInput = new Tag();
+    $searchTagInput->setTag($_POST["search"]);
+    $searchTagOutput = $searchTagInput->searchTag();
+
 ?>
 <!DOCTYPE html>
 <!-- LEGATO INDEX (FEED) -->
@@ -17,23 +22,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
     <title>Legato</title>
 </head>
 <body>
 <?php include_once("inc/nav.inc.php"); ?>
 
 <h2>Results</h2>
-<h3>Users</h3>
-<div>
+<div class="searchResult">
+    <h3>Users</h3>
     <?php foreach($searchUserOutput[0] as $key => $username): ?>
         <p><?php echo "{$username[0]}\n"; ?></p>
     <?php endforeach; ?>
 </div>
-<h3>Posts</h3>
-<div>
+<div class="searchResult">
+    <h3>Posts</h3>
     <?php foreach($searchPostOutput[0] as $key => $post): ?>
         <p><?php echo "{$post[0]}\n"; ?></p>
+    <?php endforeach; ?>
+</div>
+<div class="searchResult">
+    <h3>Tags</h3>
+    <?php foreach($searchTagOutput[0] as $key => $tag): ?>
+        <p><?php echo "{$tag[0]}\n"; ?></p>
     <?php endforeach; ?>
 </div>
 
