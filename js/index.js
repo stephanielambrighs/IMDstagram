6a58fd2b2a33381e8fb603a14e45a4f38ce3b3fe
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-20 22:26:42 +0200

    splitted logic in two functions

--- js/index.js
+++ js/index.js
@@ -22,30 +22,45 @@ postReportButtons.forEach(function(reportButton) {
     reportButton.addEventListener("click", function() {
 
         postId = reportButton.id.replace("post-", "");
-        myBody = new FormData();
-        myBody.append("postId", postId);
 
-        fetch("increaseInappropriateCounter.php", {
-            method: "POST",
-            body: myBody,
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log("Success: ", data);
-            if (data['count'] >= 3) {
-                hidePost(postId);
-            }
-        })
-        .catch((error) => {
-            console.log("Error: ", error);
-        });
+        // increate counter of the post in the database
+        increatePostCounter(postId);
+
+        // add the user and post to the reports table
+        addEntryToReportsTable(postId);
 
     });
 });
+
+
+function increatePostCounter(postId){
+    myBody = new FormData();
+    myBody.append("postId", postId);
+
+    fetch("increaseInappropriateCounter.php", {
+        method: "POST",
+        body: myBody,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Success: ", data);
+        if (data['count'] >= 3) {
+            hidePost(postId);
+        }
+    })
+    .catch((error) => {
+        console.log("Error: ", error);
+    });
+};
 
 
 function hidePost(postId){
     document.querySelector(".col-9.post-" + postId).style.display = "none";
     document.querySelector(".col-3.post-" + postId).style.display = "none";
     document.querySelector(".feed.post-" + postId).style.display = "none";
+};
+
+
+function addEntryToReportsTable(postId){
+    alert("postId: " + postId + " ; userId: " + userId);
 };
