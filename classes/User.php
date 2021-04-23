f7e83e3d55da40e997cd66697eabe70ca460ae16
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-04-23 11:13:01 +0200

    Update User.php

--- classes/User.php
+++ classes/User.php
@@ -142,7 +142,8 @@ class User
     public function searchUser(){
         $conn = Db::getConnection();
         $searchUserInput = $this->getUsername();
-        $statement = $conn->prepare("SELECT username FROM users WHERE username LIKE '%$searchUserInput%'");
+        $statement = $conn->prepare("SELECT username FROM users WHERE username LIKE :searchUserInput");
+        $statement->bindValue(':searchUserInput', $searchUserInput);
         $statement->execute();
         $searchUserOutput = array();
         $searchUserOutput[] = $statement->fetchall();
