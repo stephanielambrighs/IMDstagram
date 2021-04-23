c36eb00c8476c11e2b2fe5f1f6eb90287c709c3b
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-23 20:57:18 +0200

    added script to delete post from db

---
+++ deletePost.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once("autoload.php");
+
+if(!empty($_POST)){
+    $postId = $_POST['postId'];
+
+    // first delete reports for post to avoid constraint errors
+    $deleteReportsSuccess = Db::removeFromReports($postId);
+    if ($deleteReportsSuccess){
+
+        // now delete the post
+        $deletePostSuccess = Db::deletePost($postId);
+
+        if ($deletePostSuccess){
+            $response = [
+                'status' => 'success',
+                'message' => 'Successfully deleted post ' . $postId . ' from database'
+            ];
+        }
+        else {
+            $response = [
+                'status' => "failed",
+                'message' => 'Failed to delete post ' . $postId . ' from database'
+            ];
+        }
+
+    }else {
+        $response = [
+            'status' => "failed",
+            'message' => 'Failed to remove reports for post ' . $postId
+        ];
+    }
+
+
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
