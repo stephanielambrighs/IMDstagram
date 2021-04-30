df4d2f23f7f4b89697febd8dbc381db705ba8439
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-04-30 11:14:04 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -103,6 +103,10 @@ function addEntryToReportsTable(postId){
 document.querySelectorAll(".btn-comment").forEach(item => {
     item.addEventListener("click", function(){
         //console.log("Morgane");
+        let postId = this.dataset.postId;
+        let text = document.querySelector("#comment-text").value;
+        console.log(postId);
+        console.log(text);
         
     });
 })
