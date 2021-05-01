53a951301c4f0913a3ba5253a11cfabf2cb6bb35
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-05-01 15:18:45 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -103,10 +103,27 @@ function addEntryToReportsTable(postId){
 document.querySelectorAll(".btn-comment").forEach(item => {
     item.addEventListener("click", function(){
         //console.log("Morgane");
+        //Zoek postid en comment tekst
         let postId = this.dataset.postid;
-        let text = document.querySelector("#comment-text").value;
+        let text = this.previousElementSibling.value;
         console.log(postId);
         console.log(text);
         
+        //post naar databank (AJAX)
+        let formData = new FormData();
+        formData.append('text', text);
+        formData.append('postId', postId);
+
+        fetch('ajax/savecomment.php', {
+        method: 'POST',
+        body: formData
+        })
+        .then(response => response.json())
+        .then(result => {
+        console.log('Success:', result);
+        })
+        .catch(error => {
+        console.error('Error:', error);
+        });
     });
 })
