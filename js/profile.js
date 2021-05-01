c2a7f6971ec8bc784456436f0232c3322a6de6c7
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-05-01 11:31:51 +0200

    started on js method to call ajax script

--- js/profile.js
+++ js/profile.js
@@ -51,3 +51,30 @@ cancelProfile.addEventListener("click", function(e){
 
 
 
+let buttonPrivate = document.querySelector("#btn-private");
+buttonPrivate.addEventListener("click", function() {
+    myBody = new FormData();
+    myBody.append("userId", userId);
+
+    fetch("ajax/switchProfileStatus.php", {
+        method: "POST",
+        body: myBody,
+    })
+    .then(response => response.json())
+    .then(data => {
+        // console.log("Success: ", data);
+        console.log(data);
+        // if (data['status'] == "success"){
+        //     hidePost(postId);
+        //     showMessage(".alert.alert-success", "Successfully removed strikes for post!");
+        // }
+        // else{
+        //     showMessage(".alert.alert-danger", "Failed to remove strikes for post!");
+        // }
+        // alert(data['message']);
+    })
+    .catch((error) => {
+        console.log("Error: ", error);
+        // showMessage(".alert.alert-danger", error);
+    })
+});
