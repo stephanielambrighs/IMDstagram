15eef7bac8919a509c986cc8222577b64fd6bfca
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-05-01 21:43:33 +0200

    Update savecomment.php

--- ajax/savecomment.php
+++ ajax/savecomment.php
@@ -1,6 +1,7 @@
 <?php
     include_once(__DIR__ . "/../classes/Comment.php");
     include_once(__DIR__ . "/../classes/Db.php");
+    session_start();
 
     $user = $_SESSION['legato-user'];
     $userEmail = $user->getEmail();
@@ -10,19 +11,20 @@
     if(!empty($_POST)){
         //Nieuwe comment maken
         $c = new Comment();
-        $c->setPostId($_POST('postId'));
+        $c->setPostId($_POST('postid'));
         $c->setText($_POST('text'));
         $c->setUserId($userId); 
 
         //Comment opslaan
         $c->saveComment();
+
         //Seccues boodschap teruggeven
         $response = [
             'status' => 'success',
+            'body' => htmlspecialchars($c->getText()),
             'message' => 'Comment saved'
             
         ];
-        //'body' => htmlspecialchars($c->getText());
 
         header('Content-Type: application/json');
         echo json_encode($response);
