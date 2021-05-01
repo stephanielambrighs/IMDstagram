9e8411f59a524f162c954a034253c682fee5e2f4
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-05-01 12:51:11 +0200

    Update feed.php

--- feed.php
+++ feed.php
@@ -1,6 +1,9 @@
 <?php
     require_once("autoload.php");
     session_start();
+
+    $key = $_GET['v'];
+    //var_dump($key);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,9 +11,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php ?></title>
+    <title><?php echo $key; ?></title>
 </head>
 <body>
-    
+    <h1><?php echo $key; ?></h1>
 </body>
 </html>
