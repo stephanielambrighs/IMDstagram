088911891c3f49c0d8d9d3a755974e4ab546b443
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-05-08 09:55:51 +0200

    Update loadPosts.php

--- loadPosts.php
+++ loadPosts.php
@@ -91,7 +91,7 @@
             </div>
 
             <div class="col-3 ' . $postUniqueName .'">
-                <button type="button" class="btn btn-info"><img src="/images/like_image.png" alt="Likes">300 Likes</button>
+                <button type="button" class="btn btn-info" data-postid="'.$post->getId().'"><img src="/images/like_image.png" alt="Likes">300 Likes</button>
                 <button type="button" class="btn btn-info"><img src="/images/comment_image.png" alt="Comment">5 comments</button>
                 <button type="button" class="btn btn-info"><img src="/images/share_image.png" alt="Shares">15 shares</button>
             </div>
