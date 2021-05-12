6114ee09f8fbf86f182b014f27af2346f547216e
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-05-12 11:03:38 +0200

    Update search.php

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
     require_once("autoload.php");
 
     $users = User::search($_POST['search']);
-    $post = Post::search($_POST['search']);
+    $posts = Post::search($_POST['search']);
 
 ?>
 <!DOCTYPE html>
@@ -22,20 +22,37 @@
     <h2 class="h2">Results</h2>
     <div class="searchResult">
         <h3>Users</h3>
-        <?php foreach($searchUserOutput[0] as $key => $username): ?>
+        <?php foreach($users[0] as $key => $username): ?>
             <p><?php echo "{$username[0]}\n"; ?></p>
         <?php endforeach; ?>
     </div>
     <div class="searchResult">
         <h3>Posts</h3>
-        <?php foreach($searchPostOutput[0] as $key => $post): ?>
+        <?php foreach($posts[0] as $key => $post): ?>
             <p><?php echo "{$post[0]}\n"; ?></p>
-        <?php endforeach; ?>
-    </div>
-    <div class="searchResult">
-        <h3>Tags</h3>
-        <?php foreach($searchTagOutput[0] as $key => $tag): ?>
-            <p><?php echo "{$tag[0]}\n"; ?></p>
+            <div class="col-9">
+                <!-- <img src="https://images.pexels.com/photos/908602/pexels-photo-908602.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="user_image"> -->
+                <img src="' . $post_user_file_path . '" alt="user_image">
+                <h2>' .$user->getUsername() . '</h2>
+                <p>' . $post->getUploadedTimeAgo() .'</p>
+            </div>
+
+            <div class="feed">
+                <div class="col-4">
+                <img src="' . $post->getFile_path() . '" alt="feed">
+                </div>
+                <div class="col-6">
+                    <h3><?php echo "{$post[0]}\n"; ?></h3>
+                    <h4>Name genre</h4>
+                    <p>description</p>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <button type="button" class="btn btn-info"><img src="/images/like_image.png" alt="Likes">300 Likes</button>
+                <button type="button" class="btn btn-info"><img src="/images/comment_image.png" alt="Comment">5 comments</button>
+                <button type="button" class="btn btn-info"><img src="/images/share_image.png" alt="Shares">15 shares</button>
+            </div>
         <?php endforeach; ?>
     </div>
 </main>
