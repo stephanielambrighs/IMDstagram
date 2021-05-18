9cc1a981d298b62a61034d13c2cbf869e9e482be
MorganeVanAchter <80831190+MorganeVanAchter@users.noreply.github.com>
2021-05-18 17:42:04 +0200

    Update savecomment.php

--- ajax/savecomment.php
+++ ajax/savecomment.php
@@ -3,29 +3,24 @@
     include_once(__DIR__ . "/../classes/User.php");
     include_once(__DIR__ . "/../classes/Db.php");
 
-    //$user = $_SESSION['legato-user'];
-    //$userEmail = $user->getEmail();
-    //$userId =  Db::getUserByEmail($userEmail)->getId();
+        if(!empty($_POST)){
+            //Nieuwe comment maken
+            $userId = $_POST['userId'];
+            $postId = $_POST['postid'];
+            $text = $_POST['text'];
 
+            //Comment opslaan
+            $addCommentSuccess = Comment::saveComment($userId, $postId, $text);
+            $getCommentsPost = Comment::getAllComments($postId);
+            // $getTimeComment = Comment::getUploadedTimeAgo();
+            //$c->saveComment();
 
-    if(!empty($_POST)){
-        //Nieuwe comment maken
-        $userId = $_POST['userId'];
-        $postId = $_POST['postid'];
-        $text = $_POST['text'];
-
-        //Comment opslaan
-        $addCommentSuccess = Comment::saveComment($userId, $postId, $text);
-        $getCommentsPost = Comment::getAllComments($postId);
-        // $getTimeComment = Comment::getUploadedTimeAgo();
-        //$c->saveComment();
-
-        //Seccues boodschap teruggeven
-            $response = [
-                'text' => $text
-            ];
-        
-        header('Content-Type: application/json');
-        echo json_encode($response);
-    }
+            //Seccues boodschap teruggeven
+                $response = [
+                    'text' => $text
+                ];
+            
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
 ?>
